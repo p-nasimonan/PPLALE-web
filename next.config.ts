66b99d5ce8db4260be4d7087384f813670536bcf
p-nasimonan <pnasimonan@gmail.com>
2025-12-04 15:25:20 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,23 +6,25 @@ const config: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'pplale.pgw.jp',
-        port: '',
+        hostname: 'pplale.vercel.app',
         pathname: '/**',
       },
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pple.vr2.info',
+        pathname: '/**',
       }
     ],
-    domains: ['pplale.pgw.jp', 'localhost', 'lh3.googleusercontent.com'],
     unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -38,45 +40,11 @@ const config: NextConfig = {
   basePath: '',
   // 動的ルーティングの設定
   trailingSlash: true,
-  webpack: (config) => {
-    // SVG 以外の画像ファイルを file-loader で処理する
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/images',
-            outputPath: 'images',
-            name: '[name].[ext]',
-          },
-        },
-      ],
-    });
-
-    // SVG を React コンポーネントとして処理する
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: [{
-        loader: '@svgr/webpack',
-        options: {
-          svgo: true,
-          svgoConfig: {
-            plugins: [
-              {
-                name: 'preset-default',
-                params: {
-                  overrides: { removeViewBox: false },
-                },
-              },
-            ],
-          },
-        },
-      }],
-    });
-
-    return config;
-  }
+  turbopack: {
+    resolveAlias: {
+      '@': './src',
+    },
+  },
 };
 
 export default config;
